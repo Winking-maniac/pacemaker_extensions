3cb4ad82fd3b1b8eaaa7030a5875769a525e5c9c
Winking-maniac <gkgim@yandex.ru>
2022-06-03 18:23:16 +0300

    Telegram notifier setup fix

--- tg_notifier/setup.sh
+++ tg_notifier/setup.sh
@@ -15,6 +15,17 @@ PORT=''
 PID_DIR='/opt/pacemaker_extensions/pids/telegram_notifier/'
 LOG_DIR='/opt/pacemaker_extensions/log'
 
+log(){
+    if [ $VERBOSITY -gt $1 ]; then
+        i=0
+        while [ i -lt $1 ]; do
+            printf "\t"
+            let "i=$i + 1"
+        done
+        echo $2
+    fi
+}
+
 while [[ $# -gt 0 ]]; do
     case $1 in
         -f)
@@ -104,9 +115,9 @@ if [ $INSTALL -eq 1 ]; then
         echo 'Cannot create master directory'
         exit 1
     fi
-    if [ $VERBOSITY -gt 1 ]; then
-        echo "Master path created"
-    fi
+
+    log 1 "Master path created"
+
     chown hacluster $MASTER_PATH >/dev/null 2>&1
     cp ./telegram_notifier_master.sh $MASTER_PATH/telegram_notifier_master >/dev/null 2>&1
     if [ $? -ne 0 ]; then
@@ -114,56 +125,52 @@ if [ $INSTALL -eq 1 ]; then
         exit 1
     fi
     chown hacluster $MASTER_PATH/telegram_notifier_master >/dev/null 2>&1
-    if [ $VERBOSITY -gt 0 ]; then
-        echo "Master agent installed"
-    fi
+    chmod 755 $MASTER_PATH/telegram_notifier_master >/dev/null 2>&1
 
+    cp ./tg_notifier_maintenance.sh /usr/local/bin/notifier-maintenance
+    chmod 755 /usr/local/bin/notifier-maintenance
+
+    log 0 "Maintenance script installed"
     # Slave install
-    mkdir -p $SLAVE_PATH >/dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        echo 'Cannot create slave directory'
-        exit 1
-    fi
-    if [ $VERBOSITY -gt 1 ]; then
-        echo "Slave path created"
-    fi
-    chown hacluster $SLAVE_PATH >/dev/null 2>&1
-    cp ./telegram_notifier_slave.sh $SLAVE_PATH/telegram_notifier_slave >/dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        echo 'Cannot create slave agent'
-        exit 1
-    fi
-    chown hacluster $SLAVE_PATH/telegram_notifier_slave >/dev/null 2>&1
-    if [ $VERBOSITY -gt 1 ]; then
-        echo "Slave agent installed"
-    fi
-
-    # Master main server install
-    cp ./telegram_notifier_master.py $SLAVE_PATH/telegram_notifier_master.py >/dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        echo 'Cannot create master main server file'
-        exit 1
-    fi
-    chown hacluster $SLAVE_PATH/telegram_notifier_master.py >/dev/null 2>&1
-    if [ $VERBOSITY -gt 0 ]; then
-        echo "Master installed"
-    fi
+    # mkdir -p $SLAVE_PATH >/dev/null 2>&1
+    # if [ $? -ne 0 ]; then
+    #     echo 'Cannot create slave directory'
+    #     exit 1
+    # fi
+    # log 1 "Slave path created"
+    #
+    # chown hacluster $SLAVE_PATH >/dev/null 2>&1
+    # cp ./telegram_notifier_slave.sh $SLAVE_PATH/telegram_notifier_slave >/dev/null 2>&1
+    # if [ $? -ne 0 ]; then
+    #     echo 'Cannot create slave agent'
+    #     exit 1
+    # fi
+    # chown hacluster $SLAVE_PATH/telegram_notifier_slave >/dev/null 2>&1
+    # log 0 "Slave agent installed"
+    #
+    # # Master main server install
+    # cp ./telegram_notifier_master.py $SLAVE_PATH/telegram_notifier_master.py >/dev/null 2>&1
+    # if [ $? -ne 0 ]; then
+    #     echo 'Cannot create master main server file'
+    #     exit 1
+    # fi
+    # chown hacluster $SLAVE_PATH/telegram_notifier_master.py >/dev/null 2>&1
+    log 0 "Installation complete"
+    echo ""
 fi
 
 if [ $SETUP -eq 1 ]; then
-    if [ $VERBOSITY -gt 0 ]; then
-        echo "Started configuring"
-    fi
+    log 0 "Started configuring"
+
     # Making log dir
     mkdir -p $LOG_DIR >/dev/null 2>&1
     if [ $? -ne 0 ]; then
         echo 'Cannot create log directory'
         exit 1
     fi
-    if [ $VERBOSITY -gt 1 ]; then
-        echo "Log directory created"
-    fi
-    chown hacluster $LOG_DIR >/dev/null 2>&1
+    log 1 "Log directory created"
+
+    # chown hacluster $LOG_DIR >/dev/null 2>&1
 
     # Making pid dir
     mkdir -p $PID_DIR >/dev/null 2>&1
@@ -171,10 +178,9 @@ if [ $SETUP -eq 1 ]; then
         echo 'Cannot create pid directory'
         exit 1
     fi
-    if [ $VERBOSITY -gt 1 ]; then
-        echo "Pid directory created"
-    fi
-    chown hacluster $PID_DIR >/dev/null 2>&1
+    log 1 "Pid directory created"
+
+    # chown hacluster $PID_DIR >/dev/null 2>&1
 
     # Creating config
     if [ -z $CONFIG ]; then
@@ -185,23 +191,21 @@ if [ $SETUP -eq 1 ]; then
             echo 'Cannot create config directory'
             exit 1
         fi
-        if [ $VERBOSITY -gt 1 ]; then
-            echo "Config directory created"
-        fi
+        log 1 "Config directory created"
+
         if [ -e $DEFAULT_CONFIG -a $FORCE -eq 0 ]; then
             echo "Cannot create config by default path: file exists. Make config by yourself and provide it via --config or remove $CONFIG_DIR/telegram_notifier.ini and try again"
             exit 1
         fi
         rm $DEFAULT_CONFIG >/dev/null 2>&1
         touch $DEFAULT_CONFIG >/dev/null 2>&1
-        chmod 660 $DEFAULT_CONFIG >/dev/null 2>&1
         if [ $? -ne 0 ]; then
             echo 'Cannot create config'
+            exit 1
         fi
+        chmod 660 $DEFAULT_CONFIG >/dev/null 2>&1
 
-        if [ $VERBOSITY -gt 1 ]; then
-            echo "Config file created"
-        fi
+        log 1 "Config file created"
 
         # Reading cloud name
         while [ -z $LINE ]; do
@@ -251,33 +255,26 @@ if [ $SETUP -eq 1 ]; then
         done
         printf "\n\nYou can edit config at any time manually, for example, add new recepients. Don't forget to restart tg_notifier after config editing\n\n"
 
-        if [ $VERBOSITY -gt 0 ]; then
-            echo "Configuration complete"
-        fi
+        log 0 "Configuration complete"
+
         CONFIG=$DEFAULT_CONFIG
     else
-        if [ $VERBOSITY -gt 0 ]; then
-            echo "Using provided config"
-        fi
+        log 0 "Using provided config"
     fi
 
     # Configuring Pacemaker
-    if [ $VERBOSITY -gt 0 ]; then
-        echo "Adding notifier to Pacemaker resources"
-    fi
+    log 0 "Adding notifier to Pacemaker resources"
 
     # Virtual IP
-    pcs resource create tg_ip ocf:heartbeat:IPaddr2 ip=$IP cidr_netmask=32
+    pcs resource create tg_ip ocf:heartbeat:IPaddr2 ip=$IP cidr_netmask=32 --disabled
     if [ $? -ne 0 ]; then
         echo 'Cannot create virtual IP in Pacemaker'
         exit 1
     fi
-    if [ $VERBOSITY -gt 1 ]; then
-        echo "Virtual IP created"
-    fi
+    log 1 "Virtual IP created"
 
     # Slave configuring
-    pcs alert create path=$SLAVE_PATH/telegram_notifier_slave id=tg_notifier_slave \
+    pcs alert create path=./telegram_notifier_slave.sh id=tg_notifier_slave \
             description='Notifier slave for telegram notifier(check ocf:isp:telegram_notifier for further information)'
     if [ $? -ne 0 ]; then
         echo 'Cannot create slave agent in Pacemaker'
@@ -289,12 +286,10 @@ if [ $SETUP -eq 1 ]; then
         echo 'Cannot link slave and master agents in Pacemaker'
         exit 1
     fi
-    if [ $VERBOSITY -gt 1 ]; then
-        echo "Slave agent created"
-    fi
+    log 1 "Slave agent created"
 
     # Master configuring
-    pcs resource create tg_notifier_master ocf:isp:telegram_notifier_master script=$SLAVE_PATH/telegram_notifier_master.py config=$CONFIG piddir=$PID_DIR
+    pcs resource create tg_notifier_master ocf:isp:telegram_notifier_master script=./telegram_notifier_master.py config=$CONFIG piddir=$PID_DIR
     if [ $? -ne 0 ]; then
         echo 'Cannot create master agent in Pacemaker'
         exit 1
@@ -305,10 +300,7 @@ if [ $SETUP -eq 1 ]; then
         echo 'Cannot group IP and master agents in Pacemaker'
         exit 1
     fi
-    if [ $VERBOSITY -gt 1 ]; then
-        echo "Master agent created"
-    fi
-    if [ $VERBOSITY -gt 0 ]; then
-        echo "Installation successful"
-    fi
+    log 1 "Master agent created"
+
+    log 0 "Configuration successful"
 fi
--- tg_notifier/tg_notifier_maintenance.sh
+++ tg_notifier/tg_notifier_maintenance.sh
@@ -4,18 +4,25 @@ ACTION=""
 KEEP_YOURSELF='false'
 MESSAGE=""
 NAME="Anonymous admin"
-USAGE="usage: tg_notify_maintenance --start|--stop [--name *name*] [--keep-yourself] [--message *message*]"
+USAGE="usage: tg_notify_maintenance --start|--stop [--name *name*] [--message *message*]"
 
 while [[ $# -gt 0 ]]; do
     case $1 in
         --start)
-            ACTION="start"
+            if [ -z $ACTION ]; then
+                ACTION="start"
+            else
+                echo $USAGE
+                exit 1
+            fi
             ;;
         --stop)
-            ACTION="stop"
-            ;;
-        --keep-yourself)
-            KEEP_YOURSELF="true"
+            if [ -z $ACTION ]; then
+                ACTION="stop"
+            else
+                echo $USAGE
+                exit 1
+            fi
             ;;
         --message)
             MESSAGE=$2
@@ -44,7 +51,7 @@ if [ -z $PIDDIR ]; then
 fi
 
 if [ $ACTION == 'start' ]; then
-    printf "%s\n%s\n%s" $KEEP_YOURSELF $NAME $MESSAGE > $PIDDIR/maintenance
+    printf "%s\n%s" $NAME $MESSAGE > $PIDDIR/maintenance
 fi
 
 if [ $ACTION == 'stop' ]; then
